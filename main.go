5739e519f352f849ec42a653cef0b499f3651968
Singh <himanshi.singh@stryker.com>
2025-05-08 16:45:18 +0530

    For loop

--- main.go
+++ main.go
@@ -1,12 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-	const remainingTickets = 50
+	var remainingTickets uint = 50
+	var bookings []string
+
 	fmt.Println("Welcome to", conferenceName, "booking application")
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend.")
+
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter number of tickets")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var Names = strings.Fields(booking)
+			firstNames = append(firstNames, Names[0])
+		}
+		fmt.Printf("The firstNames of bookings are %v\n", firstNames)
+	}
 }
