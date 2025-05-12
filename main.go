42ec08dd05f57a496e1b454f143b45717c547418
Singh <himanshi.singh@stryker.com>
2025-05-12 14:27:55 +0530

    packages import export

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
+
+	"github.com/Himanshi1999/go.git/helper"
 )
 
 const conferenceTickets = 50
@@ -17,7 +19,7 @@ func main() {
 
 	for {
 		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTickets := validateUserInput(firstName, lastName, email, userTickets)
+		isValidName, isValidEmail, isValidTickets := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		if isValidName && isValidEmail && isValidTickets {
 			bookTicket(firstName, lastName, email, userTickets)
@@ -57,13 +59,6 @@ func getFirstNames() []string {
 		firstNames = append(firstNames, Names[0])
 	}
 	return firstNames
-}
-
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTickets := userTickets > 0 && userTickets <= remainingTickets
-	return isValidName, isValidEmail, isValidTickets
 }
 
 func getUserInput() (string, string, string, uint) {
